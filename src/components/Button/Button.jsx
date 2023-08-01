54bdc1a16b6bd0e26cebf0e3b625b3576874e748
Roman0064 <alee93474@gmail.com>
2023-08-01 19:15:05 +0300

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import css from './Button.module.css'
 
-export const Button = ({ onClick}) => {
+const Button = ({ onClick}) => {
     return (
       <button onClick={onClick} className={css.button}>Load More</button>
     );
@@ -11,3 +11,4 @@ export const Button = ({ onClick}) => {
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
 };
+export default Button;
