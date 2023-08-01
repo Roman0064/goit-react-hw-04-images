38f2cb42005eb1c5660c5dd0334e3ff956eb43a5
Roman0064 <alee93474@gmail.com>
2023-08-01 19:14:58 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,41 +1,38 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import css from './Modal.module.css'
 
-class Modal extends Component {
+const Modal = ({item, onClose}) => {
 
-    static propTypes = {
-        item: PropTypes.string.isRequired,
-        onClose: PropTypes.func.isRequired,
-    };
-
-    componentDidMount(){
-        window.addEventListener('keydown', this.handleKeyDown);
-    };
-
-    componentWillUnmount(){
-        window.removeEventListener('keydown', this.handleKeyDown);
-    }
-
-    handleKeyDown = e => {
-        if(e.code === 'Escape'){
-            this.props.onClose();
+    useEffect(() => {
+        const handleKeyDown = e => {
+            if(e.code === 'Escape'){
+                onClose();
+            };
         };
-    };
+        window.addEventListener('keydown', handleKeyDown);
 
-    onClickBackdrop = e => {
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        }
+    }, [onClose]);
+
+    const onClickBackdrop = e => {
         if(e.target === e.currentTarget){
-            this.props.onClose();
+            onClose();
         };
     };
 
-    render() {
-        return <div class={css.overlay} onClick={this.onClickBackdrop}>
-                <div class={css.modal}>
-                    <img src={this.props.item} alt='pictures'/>
-                </div>
+    return <div class={css.overlay} onClick={onClickBackdrop}>
+            <div class={css.modal}>
+                <img src={item} alt='pictures'/>
             </div>
-    };
+        </div>
+};
+
+Modal.propTypes = {
+    item: PropTypes.string.isRequired,
+    onClose: PropTypes.func.isRequired,
 };
 
 export default Modal;
