407ae3d5596c6b70332b4334e949792c190d49c9
Roman0064 <122211211+Roman0064@users.noreply.github.com>
2023-08-01 15:14:44 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import PropTypes from "prop-types";
+import Searchbar from "./Searchbar/Searchbar";
+import ImageGallery from './ImageGallery/ImageGallery'
+
+class App extends Component {
+  state = {
+    textSearch: '',
+  };
+
+  handleFormSubmit = textSearch => {
+    this.setState({ textSearch });
+  };
+
+  render() {
+    return(
+      <div>
+        <Searchbar onSubmit={this.handleFormSubmit}/>
+        <ImageGallery textSearch={this.state.textSearch}/>
+      </div>
+    )
+  };
 };
+
+App.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default App;
