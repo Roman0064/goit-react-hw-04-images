c9bbb9bb438c32550fe8d9b0074fb6d1f3b83375
Roman0064 <alee93474@gmail.com>
2023-08-05 11:18:19 +0300

    1

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import PropTypes from "prop-types";
 import getImages from 'services/getImages';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 import Button from '../Button/Button';
@@ -14,51 +13,47 @@ const Status = {
   REJECTED: 'rejected',
 };
 
-const ImageGallery = ({ textSearch }) => {
+const ImageGallery = ({ textSearch, page, setPage }) => {
   const [images, setImages] = useState([]);
   const [totalPages, setTotalPages] = useState(0);
   const [status, setStatus] = useState(Status.IDLE);
   const [modal, setModal] = useState({});
   const [modalShow, setModalShow] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    if(textSearch === ''){
-      return
+    const loadImages = () => {
+      setIsLoading(true);
+      getImages(textSearch, page)
+        .then(res => res.json())
+        .then(data => {
+          if (data.hits.length === 0) {
+            setStatus(Status.RESOLVED);
+            alert('No images found!');
+          } else {
+            setImages(prevState =>
+              page === 1 ? data.hits : [...prevState, ...data.hits]
+            );
+            setTotalPages(Math.ceil(data.totalHits / 12));
+            setStatus(Status.RESOLVED);
+          }
+        })
+        .catch(() => {
+          setStatus(Status.REJECTED);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
     };
-    setPage(1);
-    setImages([]);
-    loadImages();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[ textSearch ]);
 
-  const loadImages = () => {
-    setIsLoading(true);
-    getImages(textSearch, page)
-      .then(res => res.json())
-      .then(data => {
-        if (data.hits.length === 0) {
-          setStatus(Status.RESOLVED);
-          alert('No images found!');
-        } else {
-          setImages(prevState => [...prevState, ...data.hits]
-          );
-          setTotalPages(Math.ceil(data.totalHits / 12));
-          setStatus(Status.RESOLVED);
-        }
-      })
-      .catch(() => {
-        setStatus(Status.REJECTED);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  };
+    if (textSearch) {
+      loadImages();
+      return;
+    };
+  }, [textSearch, page]);
 
   const handleLoadMore = () => {
     setPage( prevState => prevState + 1 );
-    loadImages();
   };
 
   const handleImageClick = image => {
@@ -100,9 +95,4 @@ const ImageGallery = ({ textSearch }) => {
     );
   }
 };
-
-ImageGallery.propTypes = {
-  textSearch: PropTypes.string,
-};
-
 export default ImageGallery;
